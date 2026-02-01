85045ac0dc3c7ab2b289513d596906bdf41f4c77
hy3oning <shyun5635@gmail.com>
2026-02-01 19:35:52 +0900

    [create] memberService create

---
+++ src/main/java/com/hospital/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.hospital.service;
+
+import java.util.List;
+
+import com.hospital.domain.Member;
+
+public interface MemberService {
+
+	public int create(Member member) throws Exception;
+
+	public int update(Member member) throws Exception;
+
+	public int delete(Member member) throws Exception;
+
+	public List<Member> list() throws Exception;
+
+	public Member read(Member member) throws Exception;
+
+	public List<Member> search(Member member) throws Exception;
+}
---
+++ src/main/java/com/hospital/service/MemberServiceImpl.java
@@ -0,0 +1,57 @@
+package com.hospital.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hospital.domain.Member;
+import com.hospital.mapper.MemberMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	private MemberMapper mapper;
+
+	@Override
+	@Transactional
+	public int create(Member member) throws Exception {
+		return mapper.create(member);
+	}
+
+	@Override
+	@Transactional
+	public int update(Member member) throws Exception {
+		return mapper.update(member);
+	}
+
+	@Override
+	@Transactional
+	public int delete(Member member) throws Exception {
+		return mapper.delete(member);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Member> list() throws Exception {
+		return mapper.list();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Member read(Member member) throws Exception {
+		return mapper.read(member);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Member> search(Member member) throws Exception {
+		return mapper.search(member);
+	}
+
+}
