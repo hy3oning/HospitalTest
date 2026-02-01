cbaee4fd9b8f1313a95da8a0496d4b1ada2e38b2
hy3oning <shyun5635@gmail.com>
2026-02-01 18:50:42 +0900

    [create] memberMapper create

---
+++ src/main/java/com/hospital/mapper/MemberMapper.java
@@ -0,0 +1,19 @@
+package com.hospital.mapper;
+
+import java.util.List;
+
+import com.hospital.domain.Member;
+
+public interface MemberMapper {
+	public int create(Member member) throws Exception;
+	
+	public int update(Member member) throws Exception;
+	
+	public int delete(Member member) throws Exception;
+	
+	public List<Member> list() throws Exception;
+	
+	public Member read(Member member) throws Exception;
+	 
+	public List<Member> search(Member member) throws Exception;
+}
