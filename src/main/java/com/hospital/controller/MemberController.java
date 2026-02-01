73cdb5fd9eef7fca8af2b9ae77ec0a270a326bfd
hy3oning <shyun5635@gmail.com>
2026-02-01 19:46:20 +0900

    [update] member 로그인, 회원가입 컨트롤러 추가

--- src/main/java/com/hospital/controller/MemberController.java
+++ src/main/java/com/hospital/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.hospital.controller;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hospital.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
+
 
 @Controller
 @Slf4j
@@ -17,6 +19,15 @@ public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
+	
+	@GetMapping("/register")
+	public String memberRegister() {
+		return "member/register";
+	}
+	@GetMapping("/login")
+	public String memberLogin() {
+		return "member/login";
+	}
 	
 	
 }
